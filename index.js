236a4e68b251d69cdaf11262d1526b2271a25db7
rhlmalik17 <rhlmalik17@gmail.com>
2020-07-11 17:43:59 +0530

    Api Cors fix

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const { connectToDb, createUser } = require('./database')
 
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors({
+    'allowedHeaders': ['Content-Type'],
+    'origin': '*',
+    'preflightContinue': true
+  }));
 const port = 3000 || process.env.PORT;
-
 
 app.post("/api/createUser",(req,res)=>{
    createUser(req.body.email,req.body.password);
-   res.send("User Created");
+   res.set('Access-Control-Allow-Origin','*');
+   res.json({
+       message: "USER REGISTERED"
+   })
 });
 
 
-connectToDb();
+ connectToDb();
 
 app.listen(port,()=>{
     console.log("The server is running at port "+port);
